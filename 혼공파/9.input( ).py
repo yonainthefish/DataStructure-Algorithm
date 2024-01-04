a628577e591e985a3cfe6bc82b4756b3154398a7
Replit user <>
2024-01-04 02:23:21 +0000

    혼공파학습

---
+++ 혼공파/9.input( ).py
@@ -0,0 +1,13 @@
+# input 함수
+print(input("hello: "))
+a =input ("hello: "")
+print(a)
+
+input 함수의 자료형은 무조건 문자열 str! 
+
+# 문자열 -> 숫자
+1) 정수 : int("53")
+2) 부동소수점 : float("523.454")
+
+# 숫자 -> 문자
+1) c = str(a)
