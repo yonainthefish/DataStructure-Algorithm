6d12c1ece30d12117c5b1c323320b1ba87b31158
yonainthefish <yonaisgood@gmail.com>
2024-01-04 05:16:14 +0000

    혼공파학습

---
+++ 혼공파/14.is(), find(), rfind().py
@@ -0,0 +1,14 @@
+#is함수
+a = "abdc"
+print(a.ispalha()) => True
+
+
+#find() / rfind() 함수 
+=> a ="abcdabcd"
+1) find() => 왼쪽부터 탐색
+print(a.find("b")) 라면 왼쪽부터 b를 찾아냄 1!
+
+2) rfind() => 오른쪽부터 탐색
+print(a.rfind("b")) 라면 오른쪽부터 처음 찾은 b 그러나 인덱스는 앞에서부터 센다. 5! 
+
+=> 없는 연산자라면 -1을 반환
