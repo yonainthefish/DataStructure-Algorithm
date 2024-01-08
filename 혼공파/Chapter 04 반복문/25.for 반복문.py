f5157f0d25db2def88268c9fe950df6d28c052f8
yonainthefish <yonaisgood@gmail.com>
2024-01-08 11:55:56 +0900

    혼공파 폴더 정리

--- 혼공파/Chapter 04 반복문/25.for 반복문.py
+++ 혼공파/Chapter 04 반복문/25.for 반복문.py
@@ -12,13 +12,13 @@ for a_i in a:
 #예제 
 a = [1,2,3,4,5]
 
-1) 총합 
+# 1) 총합 
 sum = 0 
 for a_i in a :
   sum = sum + a_i
   print(sum)
 
-2) 총곱
+# 2) 총곱
 prod = 1
 for a_i in a :
   prod = prod * a_i
